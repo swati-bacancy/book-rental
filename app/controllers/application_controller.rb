5fc14c9bbce3a9a521f8a22afb4319cc038313f8
swati <swati.chauhan@bacancytechnology.com>
2018-03-07 18:52:15 +0530

    create current user alias method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  alias_method :current_user, :current_person
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
